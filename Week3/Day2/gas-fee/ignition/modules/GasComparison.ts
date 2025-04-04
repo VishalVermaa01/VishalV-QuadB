21b0cf7558b54d7998a4b8a62a2751708700aba2
Vishal Verma <124800033+VishalVermaa01@users.noreply.github.com>
2025-04-04 22:46:55 +0530

    Delete Week3/Day2/gas-fee directory
    "Deleted Gas-fee"

--- Week3/Day2/gas-fee/ignition/modules/GasComparison.ts
+++
@@ -1,7 +0,0 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-
-export default buildModule("GasComparisonModule", (m) => {
-  const gasComparison = m.contract("GasComparison");
-
-  return { gasComparison };
-});
